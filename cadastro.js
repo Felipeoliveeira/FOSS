4a646ad210b68e5c2d23ad1011aa3d7fd4a45603
Felipeoliveeira <Felipeoliveira.aju79@gmail.com>
2024-05-01 18:54:21 -0300

    new font

--- cadastro.js
+++ cadastro.js
@@ -1,12 +1,17 @@
 function validar(){
     let usuario = document.getElementById('usuario').value;
     let senha = document.getElementById('senha').value;
+    let subtitle = document.getElementById('sub_title');
 
     if(usuario == 'Ulisses' && senha == 'admin'){
-        alert('Login com Sucesso!');
-        location.href = "conta.html";
+        subtitle.innerText = "Login efetuado com sucesso" + ' \u{2705} '
+        setTimeout(() => {
+            location.href = "conta.html"
+        }, 1900);
     }
-    else{
-        alert('Login Incorreto!');
+    else if(usuario != 'Ulisses'){
+        subtitle.innerText = "Usuário incorreto" + '\u{1F625}'
+    } else if(senha != 'admin'){
+        subtitle.innerText = "Senha incorreta" + '\u{1F625}'
     }
 }
